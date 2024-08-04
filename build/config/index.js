37ee994f64f55a35e1fc0656f73707a8032283a7
daviddiazh <daviddiazlds1221@gmail.com>
2024-08-03 23:23:20 -0500

    .

--- build/config/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.config = void 0;
-exports.config = {
-    user: process.env.API_USER,
-    password: process.env.API_PASSWORD,
-    baseUrl: process.env.API_BASE_URL,
-};
