7135c171216cbf4e19179d94d9f22fbf322b78f2
daviddiazh <daviddiazlds1221@gmail.com>
2024-08-03 23:22:26 -0500

    .

---
+++ build/mapper/index.js
@@ -0,0 +1,28 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.MapperResponse = void 0;
+class MapperResponse {
+    constructor(payload) {
+        this.consecutivo = payload?.consecutivo;
+        this.clase = payload?.clase;
+        this.area = payload?.area;
+        this.precio = payload?.precio;
+        this.municipio = payload?.municipio;
+        this.barrio = payload?.barrio;
+        this.sostenimiento = payload?.sostenimiento;
+        this.tipo_servicio = payload?.tipo_servicio;
+        this.sala = payload?.sala;
+        this.cocina = payload?.cocina;
+        this.baños = payload?.baños;
+        this.baño_Cabina = payload?.baño_Cabina;
+        this.resumen = payload?.resumen;
+        this.video = payload?.video;
+        this.latitud = payload?.latitud;
+        this.longitud = payload?.longitud;
+        this.precio_venta = payload?.precio_venta;
+        this.sector = payload?.sector;
+        this.patios = payload?.patios;
+        this.imagenes = payload?.imagenes.map(image => ({ fotourl: image.fotourl }));
+    }
+}
+exports.MapperResponse = MapperResponse;
